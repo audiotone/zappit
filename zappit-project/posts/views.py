b8578d3d70a09e2079bea0c549c13a8158924294
audiotone <sibbreakers@gmail.com>
2022-11-23 01:50:50 +0800

    Deleting votes

--- zappit-project/posts/views.py
+++ zappit-project/posts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, mixins, status
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from .models import Post, Vote
 from .serializers import PostSerializer, VoteSerializer
 
 
-class PostList(generics.ListCreateAPIView):
+class PostList(generics.ListCreateAPIView, mixins.DestroyModelMixin):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -28,3 +29,10 @@ class VoteCreate(generics.CreateAPIView):
         if self.get_queryset().exists():
             raise ValidationError('You have already voted fo this post')
         serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('You never voted fo this post... silly!')
